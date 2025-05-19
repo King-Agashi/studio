57d00ca1a2effb46cb7935e935b20f74f88dd682
Rajarshi Agarwal <rajarshi.agarwal.10122001@gmail.com>
2025-05-19 17:23:27 +0000

    in browse books limit number of cards per row to 3 instead of 4. that is there should only be 3 columns

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -206,7 +206,7 @@ export default function BrowsePage() {
 
         <main className="md:w-3/4 lg:w-4/5">
           {filteredBooks.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {filteredBooks.map((book) => (
                 <BookCard key={book.id} book={book} />
               ))}
